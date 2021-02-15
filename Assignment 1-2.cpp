16bfd3987c54ce2aa9588a358c5afddcd60f8ed3
Hailey  Avila <hailey_avila1@my.vcccd.edu>
2021-02-15 22:49:51 +0000

    Assignment 1-2

---
+++ Assignment 1-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main();
+{
+ double Celsius, Farenheit;
+
+ cout<< "Enter the temperature by Celsius\n";
+ cin>>Celsius;
+
+ Farenheit = 9/5.0 * Celsius +32;
+
+ cout<<"Celsius"<<Celsius<<"is"<<Farenheit<<"Farenheit\n";
+}
