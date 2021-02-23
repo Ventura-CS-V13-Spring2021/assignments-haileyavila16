11b424b7504685abe4f37e5479bd0257ed3df56f
Hailey  Avila <hailey_avila1@my.vcccd.edu>
2021-02-23 18:29:39 +0000

    Fixed Assignment 2-2

--- Assignment2-2.cpp
+++ Assignment2-2.cpp
@@ -6,14 +6,10 @@ int main()
   cout << "Enter three integer values: ";
   cin >> num1 >> num2 >> num3;
   
- if (num1 == num2)
+ if (num1 == num2 && num2 == num3)
+     cout << "All numbers are the same" << endl;
+ else if (num1 == num2 || num2 == num3 || num2 == num3)
      cout << "There are two duplicated numbers" << endl;
- else if (num1 == num3)
-     cout << "There are two duplicated numbers" << endl;
- else if (num2 == num3)
-     cout << "There ae two duplicated numbers" << endl;
- else if ((num1 == num2) && (num2 == num3))
-      cout << "All numbers are the same" << endl;
  else 
       cout << "All numbers are distinct" << endl;
 
