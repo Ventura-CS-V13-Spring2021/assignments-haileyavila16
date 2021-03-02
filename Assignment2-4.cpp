61f1dd7d8526a247726ebb732ca8695a542f303f
Hailey  Avila <hailey_avila1@my.vcccd.edu>
2021-03-02 19:08:25 +0000

    Assignment 2-4

---
+++ Assignment2-4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 65;
+
+  while (i < 91)
+  {
+    if (i % 5 == 4)
+    {
+      cout << static_cast<char>(i) << " ";
+    }
+    else
+    {
+      cout << static_cast<char>(i) << " ";
+    }
+
+    i++;
+  }
+}
