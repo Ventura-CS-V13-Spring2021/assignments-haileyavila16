c731110c90ce5daff431026b9eeddb9e4ae35552
Hailey  Avila <>
2021-04-27 00:34:16 +0000

    Question 1

--- Quiz 5-2/Q1.cpp
+++ Quiz 5-2/Q1.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
   const int SIZE=10;
-  int    numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
-  int    diff[10];
-  int    sum=0;
+  int numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
+  int diff[10];
+  int sum=0;
 
-  for(int i=0;i<SIZE;i==){
+  for(int i=0;i<SIZE;i++){
     sum = sum + numbers[i];
   }
-  for()
+  for(int i=0;i<SIZE;i++){
+    diff[i] = sum-numbers[i];
+  }
+  
 }
