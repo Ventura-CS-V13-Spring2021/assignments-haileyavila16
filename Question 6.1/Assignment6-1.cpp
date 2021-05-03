5f8f39cf8fbbc60507f067b45ea3f43a3ea4fb9b
Hailey  Avila <>
2021-05-03 20:06:18 +0000

    Q 6.1

---
+++ Question 6.1/Assignment6-1.cpp
@@ -0,0 +1,26 @@
+#include "IntegarArray.hpp"
+#include <iostream>
+using namespace std;
+
+ int main()
+{
+  IntegerArray N;
+
+  N.fillUp();
+  N.printAll();
+  cout << "Current length is " << N.getLength() << endl;
+
+  cout << "After sorting: \n";
+  N.sortAsc();
+  N.printAll();
+
+  cout << "Removing the last element: ";
+  N.removeLastelm();
+  cout << "Current length is " << N.getLength() << endl;
+  N.printAll();
+
+  cout << "Append the element ";
+  N.appendElement(100);
+  cout << "Current length is " << N.getLength() << endl;
+  N.printAll();
+}
