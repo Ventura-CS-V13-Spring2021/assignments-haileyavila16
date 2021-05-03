6ef8ffa7f3b8a8004e253d2ce411ed0e6e5fb7f2
Hailey  Avila <>
2021-05-03 21:39:29 +0000

    Q3

--- Midterm/Question3.cpp
+++ Midterm/Question3.cpp
@@ -14,12 +14,20 @@ int findMin(int num1, int num2, int num3)
    return num1;
  }
  if(num2<=num1 && num2<=num3){
-   return num3;
+   return num2;
  }
+ else return num3;
  }
 }
 int findMax(int num1, int num2, int num3)
 {
-
+if(num1>=num2 && num1>=num3) {
+   return num1;
+ }
+ if(num2>=num1 && num2>=num3){
+   return num2;
+ }
+ else return num3;
+ }
 }
   
