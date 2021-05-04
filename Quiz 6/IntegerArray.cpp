d8664a054c691055182b6417666dd4877c278b1f
Hailey  Avila <>
2021-05-04 00:38:32 +0000

    Quiz 6

---
+++ Quiz 6/IntegerArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "integararray.h"
+using namespace std;
+
+int IntegarArray::getLength(void) const
+{
+  return length;
+}
+void sortArray(int flag)
+{
+
+}
+void fillUp(void)
+{
+
+}
+void getPrimenumber(void) const
+{
+
+}
+void printAll(void) const
+{
+  
+}
