5fd4e6771c2599aa7ce1f0a6ccc7f39ecc5a4c22
Hailey  Avila <>
2021-05-12 00:20:58 +0000

    Quiz 7

--- Quiz7/Rectangle.cpp
+++ Quiz7/Rectangle.cpp
@@ -9,6 +9,23 @@ Rectangle::Rectangle()
 Rectangle::Rectangle(Coordinate lbval, Coordinate rtval)
 {
   Rectangle::setLBRT(lbval, rtval);
+ //Solves for the center and the area 
   Rectangle::area = (rtval.getX() - lbval.getX()) * (rtval.getY() - lbval.getY());
-  
+  Rectangle::center = Coordinate((lbval.getX()) + (rtval.getX() - lbval.getX())) / 2, (lbval.gety() + (rtval.getY() - lbval.get()) /2);
+}
+//Gets the left bottom coordinate
+Coordinate Rectangle::getLB() const{
+  return Rectangle::lb; 
+}
+//Gets the top right coordinate
+Coordinate Rectangle::getRT() const{
+  return Rectangle::rt;
+}
+//Gets the top right coordinate
+Coordinate Rectangle::getRT() const{
+  return Rectangle::rt;
+}
+//Gets the top right coordinate
+Coordinate Rectangle::getRT() const{
+  return Rectangle::rt;
 }
