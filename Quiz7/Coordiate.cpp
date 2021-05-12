2d101d87909b1a7e979e7be8e55b5348ab9d5aec
Hailey  Avila <>
2021-05-12 00:50:34 +0000

    Quiz 7

--- Quiz7/Coordiate.cpp
+++ Quiz7/Coordiate.cpp
@@ -6,20 +6,31 @@ Coordinate::Coordinate()
 {
   Coordinate::setXY(0.0,0.0);
 }
+
 Coordinate::Coordinate(double xval, double yval)
 {
   Coordinate::setXY(xval,yval);
 }
+
 //Gets the x coordinate
 int Coordinate::getX() const{
   return x;
 }
+
 //Gets the y coordinate
 int Coordinate::getY() const{
   return y;
 }
+
+//Sets the x and y coordinates
 void Coordinate::setXY(double xval, double yval)
 {
-  
+  x = xval;
+  y = yval;
+}
 
+//Prints the x and y coordinates
+void printXY() const
+{
+  
 }
