f89b153a0889255e6fec8a4f8d979e7e40460f4c
Hailey  Avila <>
2021-05-12 00:38:42 +0000

    Quiz 7

--- Quiz7/Coordiate.cpp
+++ Quiz7/Coordiate.cpp
@@ -2,5 +2,12 @@
 #include <iostream>
 using namespace std;
 
-
+Coordinate::Coordinate()
+{
+  Coordinate::setXY(0.0,0.0);
+}
+Coordinate::Coordinate(double xval, double yval)
+{
+  Coordinate::setXY(xval,yval);
+}
 
