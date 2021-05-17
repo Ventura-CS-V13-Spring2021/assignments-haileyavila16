873a651733b13b6d2efb4735f9d4926d8890b605
Hailey  Avila <>
2021-05-17 23:34:08 +0000

    Assignment 6-1 IntegerArray.cpp

--- Question 6.1/IntegerArray.cpp
+++ Question 6.1/IntegerArray.cpp
@@ -13,5 +13,51 @@ int IntegerArray::getLastelm(){
   return numbers[length -1];
 }
 void IntegerArray::printAll(){
-  
+  if(length==0){
+    cout << "No element in the array" << endl;
+  }else{
+    cout << "Elements in the array: " << endl;
+    for(int i=0;i<length;i++){
+      cout << numbers[i] << " ";
+    }
+    cout << endl;
+  }
+}
+void IntegerArray::fillUp(){
+  length = N;
+  for(int i=0;i<N;i++){
+    numbers[i] = (rand() % 100) +1;
+  }
+}
+void IntegerArray::sortAsc(){
+  if (length==0){
+    cout << "No element in the array" << endl;
+  }
+  int ind=0,jnd=0,var=0;
+  while(ind<length-1){
+    jnd=0;
+    while(jnd<length-1-ind){
+      if(numbers[jnd+1]<numbers[jnd]){
+        var = numbers[jnd];
+        numbers[jnd] = numbers[jnd+1];
+        numbers[jnd+1] = var;
+      }
+      jnd++;
+    }
+    ind++;
+  }
+}
+void IntegerArray::removeLastelm(){
+  if(length!=0){
+    length--;
+  }else{
+    cout << "No element in the array" << endl;
+  }
+}
+void IntegerArray::appendElement(int  v){
+  if(length==N){
+    cout << "Element cannot be appended, list is full" << endl;
+    return;
+  }
+  numbers[length]=v;
 }
