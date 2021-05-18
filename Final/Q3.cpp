d64e3febbfc41be45832d991c3457ae15da7ebf8
Hailey  Avila <>
2021-05-18 22:03:55 +0000

    Q3

--- Final/Q3.cpp
+++ Final/Q3.cpp
@@ -33,7 +33,7 @@ class Student
 
     for(int i=0;i<num_classes;i++)
     {
-      class_list[i] = rhs.
+      class_list[i] = rhs.num_classes;
     }
   }
   void show()
@@ -43,8 +43,21 @@ class Student
 
     for(int i=0;i<num_classes;i++)
     {
-      cout << class_list[i] << 
+      cout << class_list[i] << num_classes;
     }
+  }
+  void reset()
+  {
+    num_classes = 0;
+    delete[]class_list;
+  }
+  Student operator = (Student &rhs)
+  {
+    Student res;
+    res.name = rhs.name;
+    res.num_classes = rhs.num_classes;
+
+
   }
 };
 
