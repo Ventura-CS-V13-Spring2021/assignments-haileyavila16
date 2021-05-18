d6a9d724e25c68a963f4cc056b4129b86f06c72a
Hailey  Avila <>
2021-05-18 19:47:14 +0000

    Q2

--- Final/Q2.cpp
+++ Final/Q2.cpp
@@ -4,4 +4,14 @@ using namespace std;
 int main()
 {
   int arr[] = {-12,3,-12,4,1,1,-12,1,-1,1,2,3,4,2,3,-12};
+  int i,j,temp;
+  int n=sizeof(arr)/sizeoff(aar[0]);
+
+  for(i=0;i<n;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+      
+    }
+  }
 }
