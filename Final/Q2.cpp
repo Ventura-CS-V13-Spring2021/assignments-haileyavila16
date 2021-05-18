888d19b3946424e8869016fdcff8f98460271cff
Hailey  Avila <>
2021-05-18 20:06:33 +0000

    Q2

--- Final/Q2.cpp
+++ Final/Q2.cpp
@@ -7,9 +7,9 @@ int main()
   int i,j,temp;
   int n=sizeof(arr)/sizeof(arr[0]);
 
-  for(i=0;i<n;i++)
+  for(i=0;i<n;++i)
   {
-    for(j=i+1;j<n;j++)
+    for(j=i+1;j<n;++j)
     {
       if(arr[i]<arr[j])
       {
@@ -19,5 +19,13 @@ int main()
       }
     }
   }
-  
+  int count=1;
+  cout << "N       Count" << endl;
+  for(int i=0;i<n-1;i++)
+  {
+    if(arr[i]==arr[i+1])
+    {
+      cout++;
+    }
+  }
 }
