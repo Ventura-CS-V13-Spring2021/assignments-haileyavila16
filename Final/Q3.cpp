9c27e3de60aa325395a6829b5254f99f5f542dff
Hailey  Avila <>
2021-05-18 21:45:50 +0000

    Q3

--- Final/Q3.cpp
+++ Final/Q3.cpp
@@ -16,6 +16,15 @@ class Student
   Student(string n, int num)
   {
     name = n;
+    num_classes = num;
+    class_list = new string[num];
+
+    for(int i=0;i<num_classes;i++)
+    {
+      cout << "Course Name: ";
+      cin >> class_list[i];
+    }
   }
+  
 };
 
