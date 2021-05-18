73d0d7a26366f22375f59853242119e890749744
Hailey  Avila <>
2021-05-18 19:57:51 +0000

    Q2

--- Final/Q2.cpp
+++ Final/Q2.cpp
@@ -5,13 +5,19 @@ int main()
 {
   int arr[] = {-12,3,-12,4,1,1,-12,1,-1,1,2,3,4,2,3,-12};
   int i,j,temp;
-  int n=sizeof(arr)/sizeoff(aar[0]);
+  int n=sizeof(arr)/sizeof(arr[0]);
 
   for(i=0;i<n;i++)
   {
     for(j=i+1;j<n;j++)
     {
-      
+      if(arr[i]<arr[j])
+      {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
     }
   }
+  
 }
